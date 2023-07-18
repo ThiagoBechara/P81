6498408dbc5d74370e6b0f7dfdaa67b2f123f4f0
ThiagoBechara <95245669+ThiagoBechara@users.noreply.github.com>
2023-07-18 16:26:34 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,28 @@
+import { Text, View, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.paragraph}>
+      espectagrama
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
